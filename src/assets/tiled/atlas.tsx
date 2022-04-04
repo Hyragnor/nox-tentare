1abbf866d24ac65e6eb10b7fab0e9ea70e4ca7b5
SaschaSt <saschast@topdesk.com>
2022-04-04 15:59:18 +0200

    add romms

---
+++ src/assets/tiled/atlas.tsx
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="atlas" tilewidth="32" tileheight="32" tilecount="144" columns="12">
+ <image source="../rooms/atlas.png" width="384" height="384"/>
+ <tile id="1">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="key" value="w"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="key" value="w"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+   <property name="key" value="w"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="key" value="x"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="key" value="x"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="key" value="x"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="key" value="f"/>
+  </properties>
+ </tile>
+</tileset>
